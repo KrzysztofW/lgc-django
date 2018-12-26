121a63213c14b21313ec33287ad371407645e563
Krzysztof Witek <kwitek@kw-net.com>
2018-12-26 17:48:01 +0100

    add login page
    Signed-off-by: Krzysztof Witek <kwitek@kw-net.com>

--- lgc/urls.py
+++ lgc/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='lgc-home'),
     path('tables/', views.tables, name='lgc-tables'),
+    path('login/', views.login, name='lgc-login'),
 ]
--- lgc/views.py
+++ lgc/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'lgc/index.html', {'title':'Home'})
 
+def login(request):
+    return render(request, 'lgc/login.html', {'titile':'Login'})
+
 def tables(request):
     return render(request, 'lgc/tables.html', {'title':'Tables'})
 
