0ce35034352cc495db517e8b7ffbc6d8f1766da4
Krzysztof Witek <kwitek@kw-net.com>
2018-12-27 18:17:00 +0100

    translate login/logout pages
    Signed-off-by: Krzysztof Witek <kwitek@kw-net.com>

--- lgc_base/urls.py
+++ lgc_base/urls.py
@@ -16,10 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
+from django.conf.urls.i18n import i18n_patterns
+from lgc import views as lgc_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='lgc-login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/login.html'), name='lgc-logout'),
-    path('', include('lgc.urls')),
 ]
+
+urlpatterns += i18n_patterns(
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='lgc-login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='lgc-logout'),
+    path('', include('lgc.urls')),
+)
