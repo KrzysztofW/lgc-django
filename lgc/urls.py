f30778ab69bbf71fb8e1c99a54a77c3ed1d72482
Krzysztof Witek <kwitek@kw-net.com>
2019-02-08 14:44:41 +0100

    lgc/urls: remove specific imports
    Signed-off-by: Krzysztof Witek <kwitek@kw-net.com>

--- lgc/urls.py
+++ lgc/urls.py
@@ -1,33 +1,34 @@
 from django.urls import path
 from . import views
-from .views import (
-    PersonCreateView, PersonListView, PersonDeleteView, PersonUpdateView,
-    ProcessCreateView, ProcessListView, ProcessDeleteView, ProcessUpdateView,
-    InitiateCase, PendingCases, UpdatePendingCase, DeletePendingCase,
-    HRCreateView, HRUpdateView, HRListView, HRDeleteView, HRCaseListView,
-)
 
 urlpatterns = [
     path('', views.home, name='lgc-home'),
 
-    path('hr-create/', HRCreateView.as_view(), name='lgc-hr-create'),
-    path('hr/<int:pk>/', HRUpdateView.as_view(), name='lgc-hr'),
-    path('hr-list/', HRListView.as_view(), name='lgc-hrs'),
-    path('hr-case-list/', HRCaseListView.as_view(), name='lgc-hr-cases'),
-    path('hr-delete/<int:pk>', HRDeleteView.as_view(), name='lgc-hr-delete'),
+    path('hr-create/', views.HRCreateView.as_view(), name='lgc-hr-create'),
+    path('hr/<int:pk>/', views.HRUpdateView.as_view(), name='lgc-hr'),
+    path('hr-list/', views.HRListView.as_view(), name='lgc-hrs'),
+    path('hr-case-list/', views.HRCaseListView.as_view(), name='lgc-hr-cases'),
+    path('hr-delete/<int:pk>', views.HRDeleteView.as_view(),
+         name='lgc-hr-delete'),
 
-    path('file-create/', PersonCreateView.as_view(), name='lgc-file-create'),
-    path('file-list/', PersonListView.as_view(), name='lgc-files'),
-    path('file-delete/<int:pk>', PersonDeleteView.as_view(), name='lgc-file-delete'),
-    path('file/<int:pk>/', PersonUpdateView.as_view(), name='lgc-file'),
+    path('file-create/', views.PersonCreateView.as_view(),
+         name='lgc-file-create'),
+    path('file-list/', views.PersonListView.as_view(), name='lgc-files'),
+    path('file-delete/<int:pk>', views.PersonDeleteView.as_view(),
+         name='lgc-file-delete'),
+    path('file/<int:pk>/', views.PersonUpdateView.as_view(), name='lgc-file'),
 
-    path('case-create/', InitiateCase.as_view(), name='lgc-case-create'),
-    path('case-list/', PendingCases.as_view(), name='lgc-cases'),
-    path('case/<int:pk>/', UpdatePendingCase.as_view(), name='lgc-case'),
-    path('case-delete/<int:pk>', DeletePendingCase.as_view(), name='lgc-case-delete'),
+    path('case-create/', views.InitiateCase.as_view(), name='lgc-case-create'),
+    path('case-list/', views.PendingCases.as_view(), name='lgc-cases'),
+    path('case/<int:pk>/', views.UpdatePendingCase.as_view(), name='lgc-case'),
+    path('case-delete/<int:pk>', views.DeletePendingCase.as_view(),
+         name='lgc-case-delete'),
 
-    path('process-create/', ProcessCreateView.as_view(), name='lgc-process-create'),
-    path('process-list/', ProcessListView.as_view(), name='lgc-processes'),
-    path('process-delete/<int:pk>', ProcessDeleteView.as_view(), name='lgc-process-delete'),
-    path('process/<int:pk>/', ProcessUpdateView.as_view(), name='lgc-process'),
+    path('process-create/', views.ProcessCreateView.as_view(),
+         name='lgc-process-create'),
+    path('process-list/', views.ProcessListView.as_view(), name='lgc-processes'),
+    path('process-delete/<int:pk>', views.ProcessDeleteView.as_view(),
+         name='lgc-process-delete'),
+    path('process/<int:pk>/', views.ProcessUpdateView.as_view(),
+         name='lgc-process'),
 ]
