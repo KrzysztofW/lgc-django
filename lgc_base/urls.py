f5a1759bc005e59a0b8425d6b34b078da14f947e
Krzysztof Witek <kwitek@kw-net.com>
2019-03-02 19:35:46 +0100

    disable admin interface
    Signed-off-by: Krzysztof Witek <kwitek@kw-net.com>

--- lgc_base/urls.py
+++ lgc_base/urls.py
@@ -19,7 +19,7 @@ from django.conf.urls.i18n import i18n_patterns
 from lgc import views as lgc_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
 ]
 
 urlpatterns += i18n_patterns(
