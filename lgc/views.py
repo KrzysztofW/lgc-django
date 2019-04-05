2a72bff291fbca70232d65c9f3e2f8b731151011
Krzysztof Witek <kwitek@kw-net.com>
2019-04-05 17:18:58 +0200

    lgc/views: add Archive Box formset
    Signed-off-by: Krzysztof Witek <kwitek@kw-net.com>

--- lgc/views.py
+++ lgc/views.py
@@ -463,6 +463,10 @@ class PersonCommonView(LoginRequiredMixin, SuccessMessageMixin):
         formsets[4].id = 'spouse_wp_id'
         formsets[4].err_msg = _("Invalid Spouse's Work Permits table")
 
+        formsets[5].title = _('Archive boxes')
+        formsets[5].id = 'ab_id'
+        formsets[5].err_msg = _('Invalid archive box number')
+
     def get_person_formsets(self):
         formsets = []
         ChildrenFormSet = modelformset_factory(lgc_models.Child,
@@ -481,6 +485,9 @@ class PersonCommonView(LoginRequiredMixin, SuccessMessageMixin):
         SpouseWorkPermitFormSet = modelformset_factory(lgc_models.SpouseWorkPermit,
                                                        form=lgc_forms.SpouseWorkPermitForm,
                                                        can_delete=True)
+        ArchiveBoxFormSet = modelformset_factory(lgc_models.ArchiveBox,
+                                                 form=lgc_forms.ArchiveBoxForm,
+                                                 can_delete=True)
 
         if self.request.POST:
             formsets.append(ChildrenFormSet(self.request.POST, prefix='children'))
@@ -488,6 +495,7 @@ class PersonCommonView(LoginRequiredMixin, SuccessMessageMixin):
             formsets.append(SpouseVisaFormSet(self.request.POST, prefix='spouse_visa'))
             formsets.append(WorkPermitFormSet(self.request.POST, prefix='wp'))
             formsets.append(SpouseWorkPermitFormSet(self.request.POST, prefix='spouse_wp'))
+            formsets.append(ArchiveBoxFormSet(self.request.POST, prefix='ab'))
             self.set_person_formsets_data(formsets)
             return formsets
 
@@ -497,12 +505,14 @@ class PersonCommonView(LoginRequiredMixin, SuccessMessageMixin):
             spouse_visas_queryset = lgc_models.SpouseVisaResidencePermit.objects.filter(person=self.object.id)
             wp_queryset = lgc_models.WorkPermit.objects.filter(person=self.object.id)
             wp_spouse_queryset = lgc_models.SpouseWorkPermit.objects.filter(person=self.object.id)
+            archive_box_queryset = lgc_models.ArchiveBox.objects.filter(person=self.object.id)
         else:
             children_queryset = lgc_models.Child.objects.none()
             visas_queryset = lgc_models.VisaResidencePermit.objects.none()
             spouse_visas_queryset = lgc_models.VisaResidencePermit.objects.none()
             wp_queryset = lgc_models.WorkPermit.objects.none()
             wp_spouse_queryset = lgc_models.WorkPermit.objects.none()
+            archive_box_queryset = lgc_models.ArchiveBox.objects.none()
         formsets.append(ChildrenFormSet(queryset=children_queryset,
                                         prefix='children'))
         formsets.append(VisaFormSet(queryset=visas_queryset,
@@ -513,6 +523,9 @@ class PersonCommonView(LoginRequiredMixin, SuccessMessageMixin):
                                           prefix='wp'))
         formsets.append(SpouseWorkPermitFormSet(queryset=wp_spouse_queryset,
                                                 prefix='spouse_wp'))
+        formsets.append(ArchiveBoxFormSet(queryset=archive_box_queryset,
+                                          prefix='ab'))
+
         self.set_person_formsets_data(formsets)
         return formsets
 
