7a7b0632764164bac00c1af2cb4637163f09e1e8
Krzysztof Witek <kwitek@kw-net.com>
2019-04-19 17:23:47 +0200

    lgc: add public urls to the role checking middleware
    Signed-off-by: Krzysztof Witek <kwitek@kw-net.com>

--- lgc/middleware.py
+++ lgc/middleware.py
@@ -3,21 +3,25 @@ from users import models as user_models
 from django.core.exceptions import PermissionDenied
 import re
 
-allowed_urls = [
-    '/', '/user/logout/', '/user/login', '/user/profile/', '/user/auth/',
+public_urls = ['/user/login',  '/user/auth/' ]
+allowed_urls = public_urls + [
+    '/', '/user/logout/', '/user/profile/',
     '/user/profile-pw-reset/', '/user/delete-profile/',
 ]
 
 class UserRolesCheck(MiddlewareMixin):
     def process_request(self, request):
         if not hasattr(request.user, 'role'):
-            return
+            for i in public_urls:
+                if request.path == '/en' + i or request.path == '/fr' + i:
+                    return
+            raise PermissionDenied
 
         if request.user.role in user_models.get_internal_roles():
             return
 
         for i in allowed_urls:
-            if request.path == '/en' + i or request.path == '/fr' + i:
+            if request.path == i or request.path == '/en' + i or request.path == '/fr' + i:
                 return
 
         if request.user.role == user_models.EMPLOYEE:
