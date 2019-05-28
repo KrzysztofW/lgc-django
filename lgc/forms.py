26627674e4225b88cccd71868d52a3bf69fdf09a
Krzysztof Witek <kwitek@kw-net.com>
2019-05-28 10:48:28 +0200

    lgc/billing: fix displaying 'with_regard_to' field
    The 'with_regard_to' field is not displayed if the form is invalid.
    In order to fix this, make the field 'readonly' instead of 'disabled'.
    
    Signed-off-by: Krzysztof Witek <kwitek@kw-net.com>

--- lgc/forms.py
+++ lgc/forms.py
@@ -370,7 +370,7 @@ class InvoiceCreateForm(forms.ModelForm):
         datepicker_set_widget_attrs(self, 'invoice_date')
         datepicker_set_widget_attrs(self, 'modification_date')
         datepicker_set_widget_attrs(self, 'po_date')
-        self.fields['with_regard_to'].widget.attrs['disabled'] = True
+        self.fields['with_regard_to'].widget.attrs['readonly'] = True
 
     class Meta:
         model = lgc_models.Invoice
