927704992e6da5197d8d51841da9d0173e7b754c
Krzysztof Witek <kwitek@kw-net.com>
2019-07-08 15:38:41 +0200

    lgc_base/settings: disable SESSION_COOKIE_SECURE by default
    SESSION_COOKIE_SECURE ensures cookie is only sent under an HTTPS connection
    but HTTPS is not enabled by default.
    
    Signed-off-by: Krzysztof Witek <kwitek@kw-net.com>

--- lgc_base/settings.py
+++ lgc_base/settings.py
@@ -17,10 +17,10 @@ from django.utils.translation import ugettext_lazy as _
 SECURE_BROWSER_XSS_FILTER=True
 SECURE_CONTENT_TYPE_NOSNIFF=True
 CSRF_COOKIE_SECURE=True
-SESSION_COOKIE_SECURE=True
 X_FRAME_OPTIONS='DENY'
 SECURE_HSTS_SECONDS=3600
 # SECURE_SSL_REDIRECT=True
+# SESSION_COOKIE_SECURE=True
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
